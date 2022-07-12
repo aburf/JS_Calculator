e917d38bc131bab33cd5b78e2a3b3170979c9fb4
Andrew <afburford@gmail.com>
2022-07-11 21:45:12 -0700

    Display calculator inputs on screen

--- calc.js
+++ calc.js
@@ -1,17 +1,49 @@
 
+calcArr = []
 
+//identify all buttons
   var allButtons = document.getElementsByTagName('button');
   console.log(allButtons);
   
-  /*
+
+
+//Add click event listener for all buttons
+function operate(){
   for(i = 0; i < allButtons.length;i++) {
-    allButtons[i].addEventListener('click', function(){
-      console.log("gubba "+ allButtons[i]);
-    });
+    allButtons[i].addEventListener('click', function (event) {
+      console.log(event.target.innerHTML);
+      calcArr.push(event.target.innerHTML);
+      console.log(calcArr);
+      let calcDisplay = document.getElementById('output')
+      calcDisplay.textContent = calcArr.join('');
+      })
+      
   }
-*/
-for(i = 0; i < allButtons.length;i++) {
-  allButtons[i].addEventListener('click', function (event) {
-    console.log(event.target.innerHTML);
-    })
+
 }
+
+operate()
+
+/*
+function operate(){
+  var allButtons = document.getElementsByTagName('button');
+
+  allButtons.forEach(btn => {
+    btn.addEventListener('click', function clickEvent(event) {
+      console.log(event.target.innerHTML);
+      calcArr.push(event.target.innerHTML);
+      console.log(calcArr)
+    });
+  });
+  
+};
+
+
+operate();
+
+*/
+
+
+
+
+
