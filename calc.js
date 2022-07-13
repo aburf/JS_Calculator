528d28f1944689e3363a2cc9919049a2392d4a8f
Andrew <afburford@gmail.com>
2022-07-12 22:15:00 -0700

    Create function to determine if click input is number

--- calc.js
+++ calc.js
@@ -6,16 +6,29 @@ operators = ['+','-','*','/']
   console.log(allButtons);
   
 
+//for taking click input and differentiating between numbers
+function isNumber(str) {
+  if (typeof str != "string") return false // we only process strings!
+  // could also coerce to string: str = ""+str
+  return !isNaN(str) && !isNaN(parseFloat(str))
+}
+
 
 //Add click event listener for all buttons
 function operate(){
   for(i = 0; i < allButtons.length;i++) {
     allButtons[i].addEventListener('click', function (event) {
       //console.log(event.target.innerHTML);
+      
+
+      let clickInput = event.target.innerHTML;
+      console.log(typeof clickInput);
+
       calcArr.push(event.target.innerHTML);
       //console.log(calcArr);
       let calcDisplay = document.getElementById('output')
       calcDisplay.textContent = calcArr.join('');
+
       })
       
   }
